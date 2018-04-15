aa6b2feb49fdaad2b625e2e609e8102857e6db9b
Thibault Le Grand <thibault.legrand@gmail.com>
2018-04-15 13:44:10 +0200

    Components created

---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Message extends React.Component {
+	render() {
+		return (
+			<div className="message">test message</div>
+			);
+	}
+}
+
+export default Message;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // Code React.js
+
+import React from 'react';
+import { render } from 'react-dom';
+
+// components
+import Connexion from './components/Connexion';
+import App from './components/App';
+
+import './index.css';
+
+
+render(
+	<App	/>, document.getElementById('root')
+	);
